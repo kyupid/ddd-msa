7563a9cccacee4e62baf142ef3d0fe07df2878de
kyupid <iamscvv@gmail.com>
2022-08-18 12:26:58 +0900

    [order] fix: OrderRequestValidator StockAdjustmentList 처리하도록 수정

--- order/src/main/java/com/kyupid/kshop/order/application/OrderRequestValidator.java
+++ order/src/main/java/com/kyupid/kshop/order/application/OrderRequestValidator.java
@@ -1,5 +1,6 @@
 package com.kyupid.kshop.order.application;
 
+import com.kyupid.kshop.order.infra.StockAdjustment;
 import com.kyupid.kshop.order.presentation.OrderRequest;
 
 import java.util.ArrayList;
@@ -10,30 +11,45 @@ import java.util.List;
  */
 public class OrderRequestValidator {
 
-    private static final String RETURN_CODE = "REQUIRED";
+    private static final String PROPERTY_REQUIRED = "REQUIRED";
+    private static final String QUANTITY_NOT_ACCEPTED = "MORE_THAN_ZERO";
 
     public List<ValidationError> validate(OrderRequest orderRequest) {
         List<ValidationError> errors = new ArrayList<>();
         if (orderRequest == null) {
-            errors.add(ValidationError.of(RETURN_CODE));
+            errors.add(ValidationError.of(PROPERTY_REQUIRED));
         } else {
             if (orderRequest.getOrdererMemberId() == null)
-                errors.add(ValidationError.of("ordererMemberId", RETURN_CODE));
+                errors.add(ValidationError.of("ordererMemberId", PROPERTY_REQUIRED));
             if (orderRequest.getStockAdjustmentList() == null)
-                errors.add(ValidationError.of("stockAdjustmentList", RETURN_CODE));
-            if (orderRequest.getStockAdjustmentList().isEmpty())
-                errors.add(ValidationError.of("stockAdjustmentList", RETURN_CODE));
+                errors.add(ValidationError.of("stockAdjustmentList", PROPERTY_REQUIRED));
+            if (orderRequest.getStockAdjustmentList().isEmpty()) {
+                errors.add(ValidationError.of("stockAdjustmentList", PROPERTY_REQUIRED));
+            } else {
+                for (StockAdjustment sd : orderRequest.getStockAdjustmentList()) {
+                    if (sd.getProductId() == null)
+                        errors.add(ValidationError.of("stockAdjustment.getProductId", PROPERTY_REQUIRED));
+                    if (sd.getAdjustmentType() == null)
+                        errors.add(ValidationError.of("stockAdjustment.getQuantity", PROPERTY_REQUIRED));
+                    if (sd.getQuantity() == null) {
+                        errors.add(ValidationError.of("stockAdjustment.getQuantity", PROPERTY_REQUIRED));
+                    } else if (sd.getQuantity() < 1) {
+                        errors.add(ValidationError.of("stockAdjustment.getQuantity", QUANTITY_NOT_ACCEPTED));
+                    }
+                }
+            }
             if (orderRequest.getDeliveryInfo() == null) {
-                errors.add(ValidationError.of("deliveryInfo", RETURN_CODE));
+                errors.add(ValidationError.of("deliveryInfo", PROPERTY_REQUIRED));
             } else {
                 if (orderRequest.getDeliveryInfo().getReceiverName() == null)
-                    errors.add(ValidationError.of("deliveryInfo.receiver", RETURN_CODE));
+                    errors.add(ValidationError.of("deliveryInfo.receiver", PROPERTY_REQUIRED));
                 if (orderRequest.getDeliveryInfo().getAddress() == null)
-                    errors.add(ValidationError.of("deliveryInfo.address", RETURN_CODE));
+                    errors.add(ValidationError.of("deliveryInfo.address", PROPERTY_REQUIRED));
                 if (orderRequest.getDeliveryInfo().getPhone() == null)
-                    errors.add(ValidationError.of("deliveryInfo.phone", RETURN_CODE));
+                    errors.add(ValidationError.of("deliveryInfo.phone", PROPERTY_REQUIRED));
             }
         }
         return errors;
     }
+
 }
