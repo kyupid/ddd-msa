f16994325e870098d66a8351470b569124a7d601
kyupid <iamscvv@gmail.com>
2022-08-22 20:40:27 +0900

    [all] add: `@EnableEurekaClient` 추가

--- member/src/main/java/com/kyupid/kshop/member/MemberApplication.java
+++ member/src/main/java/com/kyupid/kshop/member/MemberApplication.java
@@ -2,8 +2,10 @@ package com.kyupid.kshop.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MemberApplication {
 
     public static void main(String[] args) {
