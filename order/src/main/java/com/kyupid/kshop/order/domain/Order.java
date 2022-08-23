46afa6d1d994ae0de66571d3bd4c1ac3dfd0d74e
kyupid <iamscvv@gmail.com>
2022-08-23 11:41:35 +0900

    [order] add: changeOrder 배송정보 변경 추가 (2)

--- order/src/main/java/com/kyupid/kshop/order/domain/Order.java
+++ order/src/main/java/com/kyupid/kshop/order/domain/Order.java
@@ -50,7 +50,7 @@ public class Order {
     }
 
     private void setDeliveryInfo(DeliveryInfo deliveryInfo) {
-
+        this.deliveryInfo = deliveryInfo;
     }
 
     private void verifyNotYetShipped() {
